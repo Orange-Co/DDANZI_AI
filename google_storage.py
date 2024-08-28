37dcfc473b58f2b73ebc145a2a577cf9d0f94ae9
kang <kang6@ewhain.net>
2024-08-29 02:05:48 +0900

    chore: delete unused code

--- google_storage.py
+++ google_storage.py
@@ -24,6 +24,8 @@ def upload_gcs(enhance_image):
     return "https://storage.googleapis.com/" + bucket_name + "/" + enhance_image
 
 
+
+
 # Download image to Google Cloud Storage
 def download_gcs(image): 
     parsed = urlparse(image)
@@ -36,37 +38,11 @@ def download_gcs(image):
     print("버킷 정보 가져오기 성공")
     
     blob = bucket.blob(image_path)
-    print("이미지 가져오기 성공")
- 
-    return blob
-
-
-# Download image to Google Cloud Storage
-def test_download_gcs(image): 
-    parsed = urlparse(image)
-    image_path = '/'.join(parsed.path.split('/')[2:])
-    print("전처리된 이미지 경로: " + image_path)
     
-    storage_client = storage.Client()
-    print("storage_client: " + str(storage_client))
-    bucket = storage_client.bucket(bucket_name)
-    print("버킷 정보 가져오기 성공")
-    
-    blob = bucket.blob(image_path)
-    print("이미지 가져오기 성공")
-    
-    # 파일명을 추출하고 다운로드 디렉토리와 결합하여 저장 경로를 생성
     image_name = image_path.split('/')[-1]
     local_file_path = os.path.join(DOWNLOAD_DIR, image_name)
 
-    # 파일을 저장할 디렉토리가 없으면 생성
-    os.makedirs(DOWNLOAD_DIR, exist_ok=True)
-    
     blob.download_to_filename(local_file_path)
     print("이미지 다운로드 성공")
     
     return local_file_path
-
-
-if __name__ == "__main__":
-    download_gcs('ddanzi-storage', "https://storage.googleapis.com/ddanzi_bucket/test/test1-1.png")
